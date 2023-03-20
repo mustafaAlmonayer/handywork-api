125bda4ee7e3cd1c6ca719c3503e686a196887fd
mustafaAlmonayer <mustafakamal123435@gmail.com>
2023-03-20 15:22:29 +0300

    added advice for saveUser in userService

--- src/main/java/com/grad/handywork/aop/UserAspect.java
+++ src/main/java/com/grad/handywork/aop/UserAspect.java
@@ -5,15 +5,41 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.ResourceAccessException;
 
+import com.grad.handywork.entity.User;
+import com.grad.handywork.service.CloudinaryService;
 import com.grad.handywork.service.JwtService;
 
 @Aspect
 public class UserAspect {
 	
+	private final String DEFAULT_PFP_URL = "";
+	
 	@Autowired
-	JwtService jwtService;
+	private JwtService jwtService;
+	
+	@Autowired
+	private CloudinaryService cloudinaryService;
+	
+	@Autowired
+	private PasswordEncoder  passwordEncoder;
+	
+	@Pointcut("execution(* com.grad.handywork.service.UserService.saveUser(User))")
+	public void saveUserPointCut() {}
+	
+	@Before("saveUserPointCut()")
+	public void beforeSaveUser(JoinPoint joinPoint) {
+		User user = (User)joinPoint.getArgs()[0];
+		String base64File = user.getPfpFile();
+		if(base64File == null || base64File.isEmpty()) {
+			 user.setPfpFile(DEFAULT_PFP_URL);
+			 return;
+		}
+		user.setPfpUrl(cloudinaryService.imageToUrl(user.getPfpFile()));
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+	}
 	
 	@Pointcut("execution(* com.grad.handywork.controller.UserController.getUser(String, String))")
 	public void getUserPointCut() {}
--- src/main/java/com/grad/handywork/service/UserService.java
+++ src/main/java/com/grad/handywork/service/UserService.java
@@ -1,7 +1,6 @@
 package com.grad.handywork.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.grad.handywork.dto.AuthDto;
@@ -16,17 +15,9 @@ public class UserService {
 	private UserRepository userRepository;
 
 	@Autowired
-	private PasswordEncoder passwordEncoder;
-
-	@Autowired
-	private CloudinaryService cloudinaryService;
-
-	@Autowired
 	private JwtService jwtService;
 
 	public AuthDto saveUser(User user) {
-		user.setPfpUrl(cloudinaryService.imageToUrl(user.getPfpFile()));
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		User savedUser = userRepository.save(user);
 		String jwtToken = jwtService.generateToken(savedUser);
 		return AuthDto.builder().token(jwtToken).build();
