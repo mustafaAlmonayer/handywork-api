e8085bfb2e94a6004d442d026e9b17000b4f7d9c
mustafaAlmonayer <mustafakamal123435@gmail.com>
2024-08-02 09:36:19 +0300

    port 10000

--- target/generated-sources/annotations/com/grad/handywork/mapper/JobOfferMapperImpl.java
+++ target/generated-sources/annotations/com/grad/handywork/mapper/JobOfferMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-02T08:55:18+0300",
+    date = "2024-08-02T09:35:49+0300",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 21.0.4 (Amazon.com Inc.)"
 )
 @Component
